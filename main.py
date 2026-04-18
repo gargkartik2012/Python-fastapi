4ad4445a74e529bbd737d10c9f10ec31229e84e3
Kartik Garg <115023119+gargkartik2012@users.noreply.github.com>
2026-04-18 23:39:22 +0530

    crud

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-git ini
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"mesage": "There is one message for u"}
+
+
+@app.get
+def get_posts():
+    return {"data": "this is your post"}
+
+
+@app.get("/login")
+async def login():
+    return "index.js"
+
+
+@app.post("/create")
+async def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"messgase": f"tile {payload['title']}successfully created "}
